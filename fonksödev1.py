75c238b051173c78636cea1f7fdaf1ee043863be
furkan.korkmaz <furkan.korkmaz@useinsider.com>
2022-02-12 19:14:21 +0300

    12 şubat cumartesi 19.05

--- fonksödev1.py
+++ fonksödev1.py
@@ -1,13 +1,13 @@
-def bolunen_sayi_bulma(min_sayi, max_sayi, bolunecek_sayi):
-        dizi = []
-        for eleman in range(min_sayi, max_sayi+1):
-            if eleman % bolunecek_sayi == 0 :
-                dizi.append(eleman)
-        print("toplam sayı : ", len(dizi))
-        return dizi
+def bolunen_sayi_bulma(minsayi, maxsayi, boluneceksayi):
+    dizi = []
+    for eleman in range(minsayi, maxsayi + 1):
+        if eleman % boluneceksayi == 0:
+            dizi.append(eleman)
+    print("toplam sayı : ", len(dizi))
+    return dizi
+
+
 min_sayi = int(input("minimum sayı : "))
 max_sayi = int(input("maximum sayı : "))
 bolunecek_sayi = int(input("bölünecek sayı : "))
 print("bölünenlerdizisi : ", bolunen_sayi_bulma(min_sayi, max_sayi, bolunecek_sayi))
-
-
--- fonksödev2.py
+++ fonksödev2.py
@@ -1,15 +1,18 @@
-birler_bas = ["", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz"]
-onlar_bas = ["", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan"]
-inputsayi = int(input("Sayı:"))
-def sayiAtama(inputsayi):
-    return inputsayi
-def sayiOkunusu(sayi_atama):
+def sayiatama(inputsayi):
+    if inputsayi > 99 or inputsayi < 10:
+        print("lütfen geçerli format olan iki basamaklı sayı giriniz")
+    else:
+        sayi_atama = inputsayi
+        sayiokunusu(sayi_atama)
+
+
+def sayiokunusu(sayi_atama):
+    birler_bas = ["", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz"]
+    onlar_bas = ["", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan"]
     birinci = sayi_atama % 10
     ikinci = sayi_atama // 10
-    return onlar_bas[ikinci] + " " + birler_bas[birinci]
-if ( inputsayi > 99):
-      print("lütfen geçerli format  olan iki basamaklı sayı giriniz")
-elif (inputsayi < 10):
-      print("lütfen geçerli format olan iki basamaklı sayı giriniz")
-else:
-      print(sayiOkunusu(inputsayi))
+    print(onlar_bas[ikinci] + " " + birler_bas[birinci])
+
+
+sayi = int(input("Sayı:"))
+sayiatama(sayi)
