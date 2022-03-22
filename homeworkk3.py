181eff06c705fb77f79c76869ca27802b60876cb
furkan.korkmaz <furkan.korkmaz@useinsider.com>
2022-03-22 21:09:44 +0300

    3.ödev(Inheritance) 22 mart 2022 güncellendi.

--- homeworkk3.py
+++ homeworkk3.py
@@ -1,4 +1,5 @@
 class WebPush:
+
     def __init__(self, platform, optin: bool, global_frequency_capping, start_date, end_date, language, push_type):
         self.platform = platform
         self.optin = optin
@@ -13,6 +14,7 @@ class WebPush:
 
 
 class TriggerPush(WebPush):
+
     def __init__(self, trigger_page: str, platform, optin: bool, global_frequency_capping, start_date, end_date,
                  language, push_type):
         super().__init__(platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
@@ -27,6 +29,7 @@ class TriggerPush(WebPush):
 
 
 class BulkPush(WebPush):
+
     def __init__(self, send_date: int, platform, optin: bool, global_frequency_capping, start_date, end_date, language,
                  push_type):
         super().__init__(platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
@@ -41,6 +44,7 @@ class BulkPush(WebPush):
 
 
 class SegmentPush(WebPush):
+
     def __init__(self, segment_name: str, platform, optin: bool, global_frequency_capping, start_date, end_date,
                  language, push_type):
         super().__init__(platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
@@ -55,20 +59,20 @@ class SegmentPush(WebPush):
 
 
 class PriceAlertPush(WebPush):
-    def __init__(self, price_info: int, discount_rate: float, platform, optin: bool,
-                 global_frequency_capping,
-                 start_date, end_date, language, push_type):
+
+    def __init__(self, price_info: int, discount_rate: float, platform, optin: bool, global_frequency_capping,
+                  start_date, end_date, language, push_type):
         super().__init__(platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
 
-        self.discountprice = None
+        self.discounted_price = None
         self.price_info = price_info
         self.discount_rate = discount_rate
 
     def discount_price(self):
         discount = self.price_info * self.discount_rate
-        self.discountprice = self.price_info - discount
+        self.discounted_price = self.price_info - discount
 
-        return self.discountprice
+        return self.discounted_price
 
     def price_alert_push(self):
         print("**********\nPrice Information: {} \nDiscount Rate: {} \nDiscount Price: {} \nPlatform: {} \nOptin: {}"
@@ -79,6 +83,7 @@ class PriceAlertPush(WebPush):
 
 
 class InStockPush(WebPush):
+
     def __init__(self, stock_info: bool, platform, optin: bool, global_frequency_capping, start_date, end_date,
                  language, push_type):
         super().__init__(platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
@@ -97,22 +102,22 @@ class InStockPush(WebPush):
                                                       self.language, self.push_type))
 
 
-webpush1 = TriggerPush("PDP", "Desktop", True, "Bir", "01/03/22", "01/03/23", "All Language", "Trigger Push")
-webpush1.send_push()
-webpush1.trigger_push()
+WebPush1 = TriggerPush("PDP", "Desktop", True, "Bir", "01/03/22", "01/03/23", "All Language", "Trigger Push")
+WebPush1.send_push()
+WebPush1.trigger_push()
 
-webpush2 = BulkPush(5, "MOBILE", True, "İki", "01/03/22", "01/03/23", "All Language", "Bulk Push")
-webpush2.send_push()
-webpush2.bulk_push()
+WebPush2 = BulkPush(5, "MOBILE", True, "İki", "01/03/22", "01/03/23", "All Language", "Bulk Push")
+WebPush2.send_push()
+WebPush2.bulk_push()
 
-webpush3 = SegmentPush("Purchase History", "TABLET", True, "Bir", "01/03/22", "01/03/23", "Turkish", "Segment Push")
-webpush3.send_push()
-webpush3.segment_push()
+WebPush3 = SegmentPush("Purchase History", "TABLET", True, "Bir", "01/03/22", "01/03/23", "Turkish", "Segment Push")
+WebPush3.send_push()
+WebPush3.segment_push()
 
-webpush4 = PriceAlertPush(1000, 0.15, "DESKTOP", True, "Üç", "01/03/22", "01/03/23", "Spanish", "Price Alert Push")
-webpush4.send_push()
-webpush4.price_alert_push()
+WebPush4 = PriceAlertPush(1000, 0.15, "DESKTOP", True, "Üç", "01/03/22", "01/03/23", "Spanish", "Price Alert Push")
+WebPush4.send_push()
+WebPush4.price_alert_push()
 
-webpush5 = InStockPush(True, "MOBILE", True, "Beş", "01/03/22", "01/03/23", "Arabic", "In Stock Push")
-webpush5.send_push()
-webpush5.in_stock_push()
+WebPush5 = InStockPush(True, "MOBILE", True, "Beş", "01/03/22", "01/03/23", "Arabic", "In Stock Push")
+WebPush5.send_push()
+WebPush5.in_stock_push()
