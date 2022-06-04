549bcfba386c958ee2af58ea48aa14992dda00f1
furkan.korkmaz <furkan.korkmaz@useinsider.com>
2022-06-04 18:14:04 +0300

    4 haziran lcwaikiki  basic selenium ödevi

--- homework_lcwaikiki/LcwaikikiBasicSelenium.py
+++ homework_lcwaikiki/LcwaikikiBasicSelenium.py
@@ -10,41 +10,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 class LcwaikikiBasicSelenium(unittest.TestCase):
     TO_CATEGORY = (By.LINK_TEXT, "ERKEK")
     CATEGORY_PAGE = (By.CSS_SELECTOR, ".visible-md ")
+    CATEGORY_TITLE = (By.TAG_NAME, "h3")
+    SIZE_TITLE = (By.TAG_NAME, "h4")
     PRODUCT_PAGE = (By.CSS_SELECTOR, ".product-card__title ")
     CHOOSE_SIZE = (By.LINK_TEXT, "M")
     ADD_TO_CART = (By.ID, "pd_add_to_cart")
+    ADD_TO_CART_BTN = (By.ID, "pd_add_to_cart")
+    PAYMENT_STEP_BTN = (By.LINK_TEXT, "ÖDEME ADIMINA GEÇ")
     CART_PAGE = (By.ID, "shopping-cart")
     MAIN_PAGE = (By.CSS_SELECTOR, ".header__middle__left ")
-    WEBSITE = "https://www.lcwaikiki.com/tr-TR/TR"
+    website = "https://www.lcwaikiki.com/tr-TR/TR"
 
     def setUp(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
-        self.driver.get(self.WEBSITE)
+        self.driver.get(self.website)
         self.wait = WebDriverWait(self.driver, 15)
 
     def test_navigate(self):
         self.wait.until(ec.element_to_be_clickable(self.TO_CATEGORY)).click()
-        self.assertTrue(self.driver.find_element(By.CSS_SELECTOR, ".visible-lg.visible-md "))
+        self.assertTrue(self.wait.until(ec.element_to_be_clickable(self.CATEGORY_PAGE)))
 
         self.wait.until(ec.presence_of_all_elements_located(self.CATEGORY_PAGE))[4].click()
-        self.assertIn("Tişört Ve Şortlar", self.driver.find_element(By.TAG_NAME, "h3").text)
+        self.assertIn("Tişört Ve Şortlar", self.wait.until(ec.presence_of_element_located(self.CATEGORY_TITLE)).text)
 
-        self.wait.until(ec.presence_of_all_elements_located(self.PRODUCT_PAGE))[15].click()
-        self.assertIn("Beden", self.driver.find_element(By.TAG_NAME, "h4").text)
+        self.wait.until(ec.presence_of_all_elements_located(self.PRODUCT_PAGE))[3].click()
+        self.assertIn("Beden", self.wait.until(ec.presence_of_element_located(self.SIZE_TITLE)).text)
         time.sleep(4)
 
         self.wait.until(ec.element_to_be_clickable(self.CHOOSE_SIZE)).click()
-        self.assertTrue(self.driver.find_element(By.LINK_TEXT, "M"))
+        self.assertTrue(self.wait.until(ec.element_to_be_clickable(self.CHOOSE_SIZE)))
 
         self.wait.until(ec.element_to_be_clickable(self.ADD_TO_CART)).click()
-        self.assertEqual("SEPETE EKLE", self.wait.until(ec.element_to_be_clickable((By.ID, "pd_add_to_cart"))).text)
+        self.assertEqual("SEPETE EKLE", self.wait.until(ec.element_to_be_clickable(self.ADD_TO_CART_BTN)).text)
 
         self.wait.until(ec.element_to_be_clickable(self.CART_PAGE)).click()
-        self.assertEqual("ÖDEME ADIMINA GEÇ", self.driver.find_element(By.LINK_TEXT, "ÖDEME ADIMINA GEÇ").text)
+        self.assertEqual("ÖDEME ADIMINA GEÇ", self.wait.until(ec.element_to_be_clickable(self.PAYMENT_STEP_BTN)).text)
 
         self.wait.until(ec.element_to_be_clickable(self.MAIN_PAGE)).click()
-        self.assertTrue(self.driver.find_element(By.CSS_SELECTOR, ".header__middle__left "))
+        self.assertTrue(self.wait.until(ec.element_to_be_clickable(self.MAIN_PAGE)))
 
     def tearDown(self):
         self.driver.quit()
